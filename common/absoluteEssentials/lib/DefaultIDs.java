016e3e99d45322060c2aa97ec4dd506a44255aec
ZL123 <jucha00@gmail.com>
2014-01-12 10:30:27 +0000

    I dunno how many buckets fel wants...

---
+++ common/absoluteEssentials/lib/DefaultIDs.java
@@ -0,0 +1,25 @@
+package absoluteEssentials.lib;
+
+public class DefaultIDs {
+	
+	//Items
+	public static final int bucketCopper = 20140;
+	public static final int bucketTin = 20141;
+	public static final int bucketIron = 20142;
+	public static final int bucketSteel = 20143;
+	public static final int bucketCarbon = 20144;
+	public static final int bucketSilver = 20145;
+	public static final int bucketLead = 20146;
+	public static final int bucketGold = 20147;
+	public static final int bucketInvar = 20148;
+	public static final int bucketElectrum = 20149;
+	public static final int bucketTungsten = 20150;
+	public static final int bucketChromium = 20151;
+	public static final int bucketTitanium = 20152;
+	public static final int bucketAntimony = 20153;
+	public static final int bucketOsmium = 20154;
+	public static final int bucketPlatinum = 20155;
+	public static final int bucketIridium = 20156;
+	public static final int bucketTungstensteel = 20157;
+
+}
