b4ea5fa3517df530d23d67e8fc866eb366921e27
ZL123 <jucha00@gmail.com>
2014-01-12 03:04:01 +0000

    BASICALLY the initial commit...

---
+++ common/absoluteEssentials/AbsoluteEssentials.java
@@ -0,0 +1,34 @@
+package absoluteEssentials;
+
+import absoluteEssentials.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION_NUMBER, dependencies = Reference.DEPENDENCIES)
+@NetworkMod(channels = { Reference.CHANNEL_NAME }, clientSideRequired = true, serverSideRequired = false)
+public class AbsoluteEssentials {
+	
+	@Instance(Reference.MOD_ID)
+	public static AbsoluteEssentials instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+
+}
---
+++ common/absoluteEssentials/lib/Reference.java
@@ -0,0 +1,12 @@
+package absoluteEssentials.lib;
+
+public class Reference {
+	
+	public static final String MOD_ID = "AbsoluteEssentials";
+	public static final String MOD_ID_LOWERCASE = "absoluteessentials";
+    public static final String MOD_NAME = "Absolute Essentials";
+    public static final String VERSION_NUMBER = "0.0.0a";
+    public static final String CHANNEL_NAME = MOD_ID;
+    public static final String DEPENDENCIES = "required-after:Forge@[9.10.1.871,)";
+
+}
