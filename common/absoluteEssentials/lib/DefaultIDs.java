1942dea2f9973ff3f676766d6559b60e46f8535f
ZL123 <jucha00@gmail.com>
2014-01-13 17:37:54 +0000

    Durability!

--- common/absoluteEssentials/lib/DefaultIDs.java
+++ common/absoluteEssentials/lib/DefaultIDs.java
@@ -21,5 +21,39 @@ public class DefaultIDs {
 	public static final int bucketPlatinum = 20155;
 	public static final int bucketIridium = 20156;
 	public static final int bucketTungstensteel = 20157;
+	public static final int bucketZinc = 20158;
+	public static final int bucketBronze = 20159;
+	public static final int bucketBrass = 20160;
+	public static final int bucketNickel = 20161;
+	public static final int bucketMagnalium = 20162;
+	public static final int bucketAluminium = 20163;
+	public static final int bucketMagnesium = 20164;
+	
+	//Item Durability
+	public static final int copperDur = 3;
+	public static final int tinDur = 2;
+	public static final int ironDur = 5;
+	public static final int steelDur = 8;
+	public static final int carbonDur = 10;
+	public static final int silverDur = 12;
+	public static final int leadDur = 14;
+	public static final int goldDur = 7;
+	public static final int invarDur = 10;
+	public static final int electrumDur = 13;
+	public static final int tungstenDur = 16;
+	public static final int chromiumDur = 24;
+	public static final int titaniumDur = 19;
+	public static final int antimonyDur = 10;
+	public static final int osmiumDur = 32;
+	public static final int platinumDur = 36;
+	public static final int iridiumDur = 40;
+	public static final int tungstensteelDur = 48;
+	public static final int zincDur = 8;
+	public static final int bronzeDur = 7;
+	public static final int brassDur = 15;
+	public static final int nickelDur = 16;
+	public static final int magnaliumDur = 20;
+	public static final int aluminiumDur = 1;
+	public static final int magnesiumDur = 10;
 
 }
